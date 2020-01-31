388eb95e0d4c64bff40d0aec365308f21d2ab1d2
sppectre <pawelwas1997@gmail.com>
2020-01-31 03:04:19 +0100

    Lexical analyzers tests finished

--- qt/Testing/utils/Compare.h
+++ qt/Testing/utils/Compare.h
@@ -6,6 +6,7 @@
 class Compare{
 public:
     friend class simpleCompare_test;
+    friend class basicLexicalAnalyzer_test;
     virtual float compare(const std::string &FilePath1, const std::string &FilePath2) = 0; //method that gets overrided
     int numOfLines(std::ifstream& file);
     float percentage(float equalLines, int allLines);
--- qt/Testing/utils/basicLexicalAnalyzer.h
+++ qt/Testing/utils/basicLexicalAnalyzer.h
@@ -2,6 +2,7 @@
 #include "Includes.h"
 class basicLexicalAnalyzer : Compare
 {
+    friend class basicLexicalAnalyzer_test;
 public:
     float compare(const std::string &FilePath1, const std::string &FilePath2) override;
 };
